230761ce743e925860842253d478a16290d2d88c
rnori-harv <rnori@college.harvard.edu>
2023-04-28 00:39:28 -0400

    made the basic website

---
+++ src/frontend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# retrieve page
+@app.route('/retrieve', methods=['GET'])
+def retrieve():
+    return render_template('ret.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    filename = uploaded_file.filename
+    # Save the file to disk
+    uploaded_file.save(filename)
+    # Run the Python script
+    import os
+    ## 
+    os.system('python ../backend/process.py ' + filename)
+    return 'File uploaded and processed successfully.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
