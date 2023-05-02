627cfb4d21baeb5d528be17d15e5f07b9c2e6b71
rnori-harv <rnori@college.harvard.edu>
2023-05-01 20:35:32 -0400

    finished implementing IPFS for simple text file

--- src/backend/ipfs.py
+++ src/backend/ipfs.py
@@ -6,33 +6,30 @@ import threading
 from threading import Thread
 import sys
 import time
+from merkletools import MerkleTools
 import random
 
 # Define constants
-BLOCK_SIZE = 1024
+BLOCK_SIZE = 256
 # Define a class to keep track of uploaded files
 class FileStorage:
     def __init__(self):
         self._files = {}
 
-    def add_file(self, file_path):
-        file_hash = self._calculate_file_hash(file_path)
-        self._files[file_hash] = file_path
+    def add_file(self, file_content):
+        file_hash = self._calculate_file_hash(file_content)
+        self._files[file_hash] = file_content
         return file_hash
 
     def get_file(self, file_hash):
-        if file_hash in self._files:
-            return self._files[file_hash]
-        else:
-            return None
+        return self._files[str(file_hash)]
     
     def has_file(self, file_hash):
-        return file_hash in self._files
+       return str(file_hash) in list(self._files.keys())
 
-    def _calculate_file_hash(self, file_path):
-        with open(file_path, 'rb') as f:
-            file_data = f.read()
-            return hashlib.sha256(file_data).hexdigest()
+    def _calculate_file_hash(self, file_content):
+        file_data = file_content.encode('utf-8')
+        return hashlib.sha256(file_data).hexdigest()
 
 # Define a class to represent a node in the IPFS network
 class IPFSNode:
@@ -40,6 +37,7 @@ class IPFSNode:
         self._file_storage = file_storage
         self._peers = set()
         self._address =  address
+        self.dags = {}
 
     def add_peer(self, peer_address):
         self._peers.add(peer_address)
@@ -52,24 +50,36 @@ class IPFSNode:
         file_chunks = self._split_file(file_path)
         # Create a Merkle DAG for the file
         file_dag = self._create_file_dag(file_chunks)
+
+        file_hash = file_dag.get_merkle_root()
         # Add the file to the local storage
-        file_hash = self._file_storage.add_file(file_path)
-        # Broadcast the file to peers
-        self._broadcast_file(file_dag)
+        # store the merkle DAG on primary server
         return file_hash
 
     def download_file(self, file_hash, output_file_path):
-        # Look up the peers that have the file
-        peers_with_file = self._lookup_file_peers(file_hash)
-        # Request the file from each peer until successful
-        for peer_address in peers_with_file:
-            success = self._request_file_from_peer(file_hash, peer_address, output_file_path)
-            if success:
-                return True
-        return False
+        mt = self.dags[file_hash]
+        leaf_count = mt.get_leaf_count()
+        file_content = b''
+        for i in range(leaf_count):
+            leaf_hash = mt.get_leaf(i)
+            peers_w_hash = self._lookup_file_peers(leaf_hash)
+            if len(peers_w_hash) == 0:
+                print('File not found')
+                return
+            # download the file from a random peer
+            file_content += self._request_file_from_peer(leaf_hash, random.choice(peers_w_hash))
+        
+        file_content = file_content.decode('utf-8')
+        with open(output_file_path, 'w') as f:
+            f.write(file_content)
+
+        return output_file_path
+
+            
+
 
     def _split_file(self, file_path):
-        with open(file_path) as f:
+        with open(file_path, 'rb') as f:
             chunks = []
             while True:
                 block = f.read(BLOCK_SIZE)
@@ -79,39 +89,38 @@ class IPFSNode:
             return chunks
 
     def _create_file_dag(self, chunks):
-        node_list = []
         for chunk in chunks:
-            chunk_hash = hashlib.sha256(chunk).hexdigest()
-            node_list.append({'Data': chunk, 'Hash': chunk_hash, 'Links': []})
-            # send chunk to a random peer
             self._send_file_to_peer(chunk, random.choice(list(self._peers)))
-        while len(node_list) > 1:
-            new_node_list = []
-            for i in range(0, len(node_list), 2):
-                left_node = node_list[i]
-                right_node = node_list[i+1] if i+1 < len(node_list) else left_node
-                combined_data = left_node['Data'] + right_node['Data']
-                combined_hash = hashlib.sha256(combined_data).hexdigest()
-                combined_node = {'Data': combined_data, 'Hash': combined_hash, 'Links': [
-                    {'Name': 'left', 'Hash': left_node['Hash']},
-                    {'Name': 'right', 'Hash': right_node['Hash']}
-                ]}
-                new_node_list.append(combined_node)
-            node_list = new_node_list
-        return node_list[0]
+        chunks = [chunk.decode('utf-8') for chunk in chunks]
+        mt = MerkleTools(hash_type = 'sha256')
+        mt.add_leaf(chunks, True)
+        mt.make_tree()
+        self.dags[mt.get_merkle_root()] = mt
+        return mt
 
-    def _broadcast_file(self, file_dag):
-        file_dag_json = json.dumps(file_dag)
-        for peer_address in self._peers:
-            self._send_file_to_peer(file_dag_json, peer_address)
-
-    def _send_file_to_peer(self, file_dag_json, peer_address):
+    def _send_file_to_peer(self, chunk, peer_address):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect(peer_address)
+                s.settimeout(10)  # Set a timeout of 10 seconds
                 s.sendall(b'UPLOAD')
-                s.sendall(bytes(file_dag_json, 'utf-8'))
-                s.recv(BLOCK_SIZE)  # Wait for peer to acknowledge receipt
+                try:
+                    s.recv(BLOCK_SIZE)  # Wait for peer to acknowledge upload
+                except socket.timeout:
+                    print("Timed out waiting for COMMAND CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
+                try:
+                    s.sendall(chunk)
+                    time.sleep(0.1)
+                    s.sendall(b'DONE')
+                    s.recv(BLOCK_SIZE)  # Wait for peer to acknowledge receipt of chunk
+                except socket.timeout:
+                    print("Timed out waiting for UPLOAD CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
+                finally:
+                    s.close()
             except:
                 print('Failed to send file to peer: {}'.format(peer_address))
 
@@ -126,32 +135,59 @@ class IPFSNode:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect(peer_address)
+                s.settimeout(10)  # Set a timeout of 10 seconds
                 s.sendall(b'LOOKUP')
-                s.sendall(bytes(file_hash, 'utf-8'))
-                response = s.recv(BLOCK_SIZE).decode('utf-8')
-                if response == 'FOUND':
-                    return True
-                else:
-                    return False
+                try:
+                    s.recv(BLOCK_SIZE)  # Wait for peer to acknowledge lookup
+                except socket.timeout:
+                    print("Timed out waiting for LOOKUP CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
+                try:
+                    s.sendall(bytes(file_hash, 'utf-8'))
+                    time.sleep(0.1)
+                    s.sendall(b'DONE')
+                    response = s.recv(BLOCK_SIZE)
+                    if response == b'FOUND':
+                        return True
+                    else:
+                        return False
+                except socket.timeout:
+                    print("Timed out waiting for LOOKUP CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
             except:
+                print("FAILING LOOKUP")
                 return False
 
-    def _request_file_from_peer(self, file_hash, peer_address, output_file_path):
+    def _request_file_from_peer(self, file_hash, peer_address):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect(peer_address)
                 s.sendall(b'DOWNLOAD')
-                s.sendall(bytes(file_hash, 'utf-8'))
-                file_dag_json = b''
-                while True:
-                    block = s.recv(BLOCK_SIZE)
-                    if not block:
-                        break
-                    file_dag_json += block
-                file_dag = json.loads(file_dag_json.decode('utf-8'))
-                self._retrieve_file_from_dag(file_dag, output_file_path)
-                return True
+                try:
+                    s.recv(BLOCK_SIZE)  # Wait for peer to acknowledge download
+                except socket.timeout:
+                    print("Timed out waiting for DOWNLOAD CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
+                try:
+                    s.sendall(bytes(file_hash, 'utf-8'))
+                    time.sleep(0.1)
+                    s.sendall(b'DONE')
+                    file_content = b''
+                    while True:
+                        block = s.recv(BLOCK_SIZE)
+                        if block == b'DONE':
+                            break
+                        file_content += block
+                    return file_content
+                except socket.timeout:
+                    print("Timed out waiting for DOWNLOAD CONFIRMATION from peer")
+                    s.close()
+                    sys.exit(1)
             except:
+                print("FAILING DOWNLOAD")
                 return False
 
     def _retrieve_file_from_dag(self, file_dag, output_file_path):
@@ -165,16 +201,13 @@ class IPFSNode:
         with open(output_file_path, 'wb') as f:
             for chunk in chunks:
                 f.write(chunk)
-    ## add logic for peers to do all of the requested actions
 
     def _handle_peer(self, conn, addr):
         with conn:
-            print('Connected by', addr)
             while True:
                 data = conn.recv(BLOCK_SIZE)
                 if not data:
                     break
-                print('Received', repr(data))
                 if data == b'CLIENTUPLOAD':
                     self._handle_client_upload(conn)
                 elif data == b'CLIENTDOWNLOAD':
@@ -190,6 +223,7 @@ class IPFSNode:
     
     
     def _handle_client_upload(self, conn):
+        time.sleep(0.5)
         filename = b''
         while True:
             block = conn.recv(BLOCK_SIZE)
@@ -214,21 +248,25 @@ class IPFSNode:
         conn.sendall(b'ACK')
 
     def _handle_upload(self, conn):
-        file_dag_json = b''
+        conn.sendall(b'ACK')
+        file_content = b''
         while True:
             block = conn.recv(BLOCK_SIZE)
-            if not block:
+            if block == b'DONE':
                 break
-            file_dag_json += block
-        file_dag = json.loads(file_dag_json.decode('utf-8'))
-        self._file_storage.add_file(file_dag)
-        conn.sendall(b'ACK')
+            file_content += block
+        hash = self._file_storage.add_file(file_content.decode('utf-8'))
+        print('File hash: {}'.format(hash))
+        print(str(self._address) + ': File added to storage')
+        #print(self._file_storage.get_file(hash))
+        conn.sendall(b'DONE')
     
     def _handle_lookup(self, conn):
+        conn.sendall(b'ACK')
         file_hash = b''
         while True:
             block = conn.recv(BLOCK_SIZE)
-            if not block:
+            if block == b'DONE':
                 break
             file_hash += block
         file_hash = file_hash.decode('utf-8')
@@ -238,16 +276,18 @@ class IPFSNode:
             conn.sendall(b'NOT_FOUND')
     
     def _handle_download(self, conn):
+        conn.sendall(b'ACK')
         file_hash = b''
         while True:
             block = conn.recv(BLOCK_SIZE)
-            if not block:
+            if block == b'DONE':
                 break
             file_hash += block
         file_hash = file_hash.decode('utf-8')
-        file_dag = self._file_storage.get_file(file_hash)
-        file_dag_json = json.dumps(file_dag)
-        conn.sendall(bytes(file_dag_json, 'utf-8'))
+        file_content = self._file_storage.get_file(file_hash)
+        conn.sendall(file_content.encode('utf-8'))
+        time.sleep(0.1)
+        conn.sendall(b'DONE')
     
     def _start_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -279,4 +319,22 @@ def serve(port):
     # take the test.txt file in frontend/ and upload it to the network
     if port == 50051:
         file_hash = ipfs_node.upload_file('test.txt')
+        # hash the file with SHA-256 and print it
+        filename = 'test.txt'
+        with open(filename, 'rb') as f:
+            # Create a SHA-256 hash object
+            sha256 = hashlib.sha256()
+            
+            # Read the file in chunks and update the hash object
+            while True:
+                data = f.read(4096)
+                if not data:
+                    break
+                sha256.update(data)
+
+        # Print the SHA-256 hash as a hexadecimal string
+
         print('Uploaded file with hash: {}'.format(file_hash))
+        # download the file from the network
+        output_file_path = file_hash + '.txt'
+        ipfs_node.download_file(file_hash, output_file_path)
